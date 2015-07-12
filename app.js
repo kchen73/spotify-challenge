cd8bb74034f54c36722ccc3c877afe3b2041074a
unknown <kchen73@uw.edu>
2015-07-12 04:41:01 -0700

    big changes

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var data;
 var baseUrl = 'https://api.spotify.com/v1/search?type=track&limit=1&q='
 var myApp = angular.module('myApp', [])
+var data1;
+var data2;
 
-var myCtrl = myApp.controller('myCtrl', function($scope, $http, $q) {
-  $scope.audioObject = {}
-  var one = $http.get(baseUrl + $scope.track)
-  var two = $http.get(baseUrl + $scope.track2) 
-  $q.all([one, two]).then(function(response) {
-      data = $scope.tracks = response.tracks.items
-  })
+var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
+  $scope.audioObject = {}  
+  $scope.getSongs = function() {
+    $http.get(baseUrl + $scope.track).success(function(response) {
+      data1 = $scope.tracks = response.tracks.items
+  $scope.getSongs2 = function() {
+    $http.get(baseUrl + $scope.track2).success(function(response) {
+      data2 = $scope.tracks = response.tracks.items
+    })
+  }
 
- 
 
   $scope.play = function(song) {
     if($scope.currentSong == song) {
@@ -25,6 +28,7 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http, $q) {
       $scope.currentSong = song
     }
   }
+
 })
 
 
